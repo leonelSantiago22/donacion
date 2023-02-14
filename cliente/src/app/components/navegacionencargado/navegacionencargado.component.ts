69509c2ee62f823fe4965d1f02a629a1752c0221
leonelSantiago22 <santiago.rosas.leonel@gmail.com>
2023-02-13 19:51:12 -0600

    Terminado solo falta el manual de usuario

--- cliente/src/app/components/navegacionencargado/navegacionencargado.component.ts
+++ cliente/src/app/components/navegacionencargado/navegacionencargado.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
-
+import { HospitalService } from 'src/app/services/hospital.service';
+import { EnfermeraService } from 'src/app/services/enfermera.service';
+import { BancoService } from 'src/app/services/banco.service';
+import { Hospital } from 'src/app/models/hospital';
+import { Enfermera } from 'src/app/models/enfermera';
+import { Bancos } from 'src/app/models/banco';
+declare var $ : any;
 @Component({
   selector: 'app-navegacionencargado',
   templateUrl: './navegacionencargado.component.html',
   styleUrls: ['./navegacionencargado.component.css']
 })
 export class NavegacionencargadoComponent {
+  agregarHospitales : any;
+  enfermerasAgregar:any;
+  bancosAgregar : any;
+  constructor(private hospitalService:HospitalService,
+              private enfermeraService:EnfermeraService,
+              private bancoService:BancoService)
+      {}
 
+      //elementos para hospital
+      agregarHospital()
+      {
+        this.hospitalService.agregarHospital(this.agregarHospitales).subscribe((resHospital: any) => {
+          console.log(resHospital);
+      },
+          (err: any) => console.error(err)
+        );
+      }
+      prepararAgregarHospital()
+      {
+        $('#mymodalAgregarHospital').modal({
+          dismissible: false
+          });
+          $('#mymodalAgregarHospital').modal('open');
+      } 
+      ngOnInit()
+      {
+        $(document).on('focus','.dropdown-trigger',function(){
+          $('.dropdown-trigger').dropdown();
+          });
+          this.agregarHospitales = new Hospital();
+          this.enfermerasAgregar = new Enfermera();
+          this.bancosAgregar = new Bancos();
+      }
+
+      agregarEnfermera()
+      {
+        this.enfermeraService.agregarEnfermera(this.enfermerasAgregar).subscribe((resEnfermera: any) => {
+          console.log(resEnfermera);
+          this.enfermerasAgregar = resEnfermera;
+          },
+          (err: any) => console.error(err)
+        );
+      }
+      prepararAgregarEnfermera(){
+      $('#mymodalAgregarEnfermera').modal({
+            dismissible: false
+      });
+      $('#mymodalAgregarEnfermera').modal('open');
+      }
+      agregarBanco()
+        {
+          this.bancoService.agregarBanco(this.bancosAgregar).subscribe((resBanco: any) => {
+            console.log(resBanco);
+            this.bancosAgregar  = resBanco;
+        },
+            (err: any) => console.error(err)
+          );
+        }
+        prepararAgregarBanco(){
+          $('#mymodalAgregarBanco').modal({
+                dismissible: false
+          });
+          $('#mymodalAgregarBanco').modal('open');
+        }
+        
 }
